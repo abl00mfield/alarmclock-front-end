389b3a99da7fc3845884d5c896561bd2fa328340
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-19 18:55:34 -0600

    added hamburger menu for navbar small screen sizes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,43 +6,71 @@ import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   const { user, setUser } = useContext(UserContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = (event) => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const handleSignOut = () => {
     localStorage.removeItem("token");
+    setIsMenuOpen(false);
     setUser(null);
   };
 
   return (
     <nav className={styles.container}>
       {user ? (
-        <div className={styles.navContainer}>
-          <ul>
+        <>
+          <div className={styles.navContainer}>
+            <div className={styles.navLeft}>
+              <button className={styles.hamburger} onClick={toggleMenu}>
+                ☰
+              </button>
+              <ul
+                className={`${styles.navLinks} ${
+                  isMenuOpen ? styles.show : ""
+                }`}
+              >
+                <li>
+                  <img
+                    src="/images/alarm-clock.png"
+                    alt="alarm clock"
+                    width="30px"
+                  />
+                </li>
+                <li>Welcome - {user.username}</li>
+                <li>
+                  <Link to="/" onClick={() => setIsMenuOpen(false)}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/alarms" onClick={() => setIsMenuOpen(false)}>
+                    Your Alarms
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={handleSignOut}>
+                    Sign Out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <ThemeToggle />
+          </div>
+        </>
+      ) : (
+        <ul className={styles.navContainer}>
+          <div className={styles.home}>
             <li>
+              {" "}
               <img
                 src="/images/alarm-clock.png"
                 alt="alarm clock"
                 width="30px"
               />
             </li>
-            <li>Welcome - {user.username}</li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/alarms">Your Alarms</Link>
-            </li>
-            <li>
-              <Link to="/" onClick={handleSignOut}>
-                Sign Out
-              </Link>
-            </li>
-          </ul>
-          <ThemeToggle />
-        </div>
-      ) : (
-        <ul className={styles.navContainer}>
-          <div className={styles.home}>
-            <li>Alarm Mate</li>
             <li>
               <Link to="/">Home</Link>
             </li>
